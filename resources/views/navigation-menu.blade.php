c4011c2057c6e4adf39593398d6ae1308ddf6a57
Дима Филипенко <dimafilipenko@admins-MacBook-Pro.local>
2022-03-05 12:52:01 +0200

    hide users

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,9 +16,9 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
 
-                    <x-jet-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
-                        {{ __('Users') }}
-                    </x-jet-nav-link>
+{{--                    <x-jet-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">--}}
+{{--                        {{ __('Users') }}--}}
+{{--                    </x-jet-nav-link>--}}
                     <x-jet-nav-link href="{{ route('telegram-users.index') }}" :active="request()->routeIs('telegram-users.index')">
                         {{ __('telegram-users') }}
                     </x-jet-nav-link>
